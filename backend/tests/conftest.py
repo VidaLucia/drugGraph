3f453cb9fb370545effa7dc8e20294ed4ab1ec8a
Kevin Du <kdu22@uwo.ca>
2026-09-02 12:20:14 -0400

    made a conftest

---
+++ backend/tests/conftest.py
@@ -0,0 +1,48 @@
+import pytest_asyncio
+
+from sqlalchemy import delete
+from sqlalchemy.pool import NullPool
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+
+from app.models.drug_entity import (
+    DrugEntity,
+    DrugRelationshipEntity,
+)
+
+
+TEST_DATABASE_URL = (
+    "postgresql+asyncpg://postgres:postgres@localhost:5432/druggraph_test"
+)
+
+test_engine = create_async_engine(
+    TEST_DATABASE_URL,
+    echo=False,
+    poolclass=NullPool,
+)
+
+TestSessionLocal = async_sessionmaker(
+    bind=test_engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+)
+
+
+@pytest_asyncio.fixture
+async def db_session():
+    async with TestSessionLocal() as session:
+
+        await session.execute(
+            delete(DrugRelationshipEntity)
+        )
+
+        await session.execute(
+            delete(DrugEntity)
+        )
+
+        await session.commit()
+
+        yield session
